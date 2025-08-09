8751e100c017befdcef348849af2b6d6c5438194
mufaddal-lashkar <mufaddal.github@gmail.com>
2025-08-09 17:04:44 +0530

    first commit

---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Details = () => {
+
+    return (
+        <View>
+            <Text>Movie Details screen</Text>
+        </View>
+    )
+}
+
+export default Details
